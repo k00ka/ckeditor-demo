181a8b767154a36e6d7e7404e0e07e441a1c7b53
David Andrews <david@ryatta.com>
2016-12-29 10:43:18 -0500

    Scaffold app, compatible with MentorCity.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CkeditorDemo::Application.config.secret_token = '17b1a25f92e2e266b4ee67e6d13ab6ba8c59950793e8fdc74d5fcec08e3cb31a96e8b3846746eb093cd8e2090d72a0b608f091567af32459b7aa729356880c80'
