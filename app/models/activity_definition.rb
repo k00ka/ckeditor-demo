c1f8d7fd95b042859fd3f747d79b7e4f93c481df
Chris Larsen <clarsenipod@gmail.com>
2016-12-29 14:28:25 -0500

    Finish Activity Definition

--- app/models/activity_definition.rb
+++ app/models/activity_definition.rb
@@ -1,7 +1,11 @@
 class ActivityDefinition < ActiveRecord::Base
   attr_accessible :html_blob
 
-  def name
+  def short_description
     self.html_blob.gsub(/<[^>]*>/, " ").slice(0..20)
+  end
+
+  def name_inputs
+    self.html_blob.gsub(/\sname="([^"]*)"/, ' name="activity[responses][\1]"')
   end
 end
