ffd644ae9a76a666f91b608fc3454a529d5bef92
Chris Larsen <clarsenipod@gmail.com>
2016-12-29 11:13:28 -0500

    Add before_filter

--- app/controllers/activity_definitions_controller.rb
+++ app/controllers/activity_definitions_controller.rb
@@ -1,26 +1,15 @@
 class ActivityDefinitionsController < ApplicationController
-  before_action :set_definition, only: [:edit, :show, :destroy]
+  before_filter :set_definition, only: [:edit, :show, :destroy]
 
   def index
     @activity_definitions = ActivityDefinition.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-    end
   end
 
   def show
-    respond_to do |format|
-      format.html # show.html.erb
-    end
   end
 
   def new
     @activity_definition = ActivityDefinition.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-    end
   end
 
   def edit
@@ -29,22 +18,18 @@ class ActivityDefinitionsController < ApplicationController
   def create
     @activity_definition = ActivityDefinition.new(params[:activity_definition])
 
-    respond_to do |format|
-      if @activity_definition.save
-        format.html { redirect_to @activity_definition, notice: 'Activity definition was successfully created.' }
-      else
-        format.html { render action: "new" }
-      end
+    if @activity_definition.save
+      redirect_to @activity_definition, notice: 'Activity definition was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   def update
-    respond_to do |format|
-      if @activity_definition.update_attributes(params[:activity_definition])
-        format.html { redirect_to @activity_definition, notice: 'Activity definition was successfully updated.' }
-      else
-        format.html { render action: "edit" }
-      end
+    if @activity_definition.update_attributes(params[:activity_definition])
+      redirect_to @activity_definition, notice: 'Activity definition was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
