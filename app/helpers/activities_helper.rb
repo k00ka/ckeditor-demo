352b7c551a6ea52e12dbcf0045e20b23531f2e07
David Andrews <david@ryatta.com>
2017-01-24 12:12:07 -0500

    Minor refactoring

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,18 @@
 module ActivitiesHelper
+  def activity_html(activity)
+    return activity.activity_definition.html_blob
+    tags = activity.activity_definition.html_blob.split("<")
+    output = ""
+    #capture do
+      output += tags.shift
+      tags.each do |tag|
+        output += "<#{tag}" and next unless tag.split(" ").first == "input"
+        name = tag.match(/.*name="([^"]*)".*/)[1]
+        value = activity.responses[name] || ""
+        the_rest = tag.sub(/input/,"").sub(/ name="[^"]*"/, "").sub(/ value="[^"]*"/, "")
+        output += "<input name=\"activity[responses][#{name}]\" value=\"#{value}\" #{the_rest}"
+      end
+    #end
+    output
+  end
 end
